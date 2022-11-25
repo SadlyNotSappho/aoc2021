5c82731cbaed8f13a3f5eb2412d515a1e84947cc
SapphicSapphire <112831566+SapphicSapphire@users.noreply.github.com>
2022-11-25 13:53:29 -0800

    optimize day1 part1

--- src/day1.rs
+++ src/day1.rs
@@ -1,31 +1,14 @@
 pub fn part1(input: String) {
     let vec: Vec<u32> = input.trim().split('\n').map(|n| n.parse::<u32>().unwrap()).collect();
-    let mut answers: Vec<DayOneDepth> = vec![];
-    let mut answer_vec = vec![];
+    let mut answers = vec![];
 
     for (pos, _) in vec.iter().enumerate() {
         if pos != 0 {
             if vec[pos] >= vec[pos - 1] {
-                answers.push(DayOneDepth::Deeper);
-            } else if vec[pos] == vec[pos - 1] {
-                answers.push(DayOneDepth::Same)
-            } else {
-                answers.push(DayOneDepth::Shallower)
+                answers.push(true);
             }
         }
     };
 
-    for a in answers {
-        if a == DayOneDepth::Deeper {
-            answer_vec.push(true);
-        }
-    };
-
-    println!("{:?}", answer_vec.len())
-}
-#[derive(PartialEq, Eq, Clone, Copy)]
-enum DayOneDepth {
-    Deeper,
-    Shallower,
-    Same
+    println!("{:?}", answers.len())
 }
